8aaebf4f11cb38272bfc4760a1aebbd27797b671
screenflicker <accounts@screenflicker.com>
2025-02-16 18:38:46 -0800

    feat: add react-query

--- packages/frontend/src/routes/root.tsx
+++ packages/frontend/src/routes/root.tsx
@@ -2,7 +2,7 @@ import { Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 import Header from '../components/Header/Header';
 import { Nav } from '../components/Nav/Nav';
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export function Root() {
   return (
@@ -11,7 +11,7 @@ export function Root() {
       <Nav />
       <Outlet />
       <TanStackRouterDevtools />
-      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+      <ReactQueryDevtools initialIsOpen={false} />
     </div>
   );
 }
